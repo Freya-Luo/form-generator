ea25fdfdb5344f21d1ba0acaa91be85195b52753
Freya-Luo <y233luo@gmail.com>
2022-07-02 11:07:00 +0200

    add: setup project build configuration

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,15 @@ const { defineConfig } = require("@vue/cli-service");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 const CircularDependencyPlugin = require("circular-dependency-plugin");
 
+const isPackLib = process.env.TYPE === "packLib";
+
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack(config) {
-    config.plugin("manaco").use(new MonacoWebpackPlugin());
+    // monaco plugin is used when building "template/"", no need if we are building "lib/""
+    if (!isPackLib) {
+      config.plugin("manaco").use(new MonacoWebpackPlugin());
+    }
     config.plugin("circular").use(new CircularDependencyPlugin());
   },
 });
