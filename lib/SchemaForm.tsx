c46f29bcc8b3c37611fb4411bd41126902a0baed
Freya-Luo <y233luo@gmail.com>
2022-07-12 20:28:00 +0200

    fix: control the result of multiple consecutive validation calls

--- lib/SchemaForm.tsx
+++ lib/SchemaForm.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, PropType, provide, Ref, shallowRef, watch, watchEffect } from "vue";
+import { defineComponent, PropType, provide, Ref, shallowRef, ref, watch, watchEffect } from "vue";
 import { Schema } from "./types";
 import SchemaItem from "./SchemaItem";
 import { SchemaFormContextKey } from "./context";
@@ -8,7 +8,6 @@ import { validateFormData, ErrorSchema } from "./validator";
 // Ajv necessary configurations setup
 const defaultAjvOptions: Options = {
   allErrors: true,
-  jsonPointers: true,
 };
 
 interface ContextRef {
@@ -63,24 +62,49 @@ export default defineComponent({
         ...props.ajvOptions,
       });
     });
-
+    // transform to error schema
     const errorSchemaRef: Ref<ErrorSchema> = shallowRef({});
+
+    /* Control the async validation result (kind of like throttling control) */
+    const resolvedValidationRef = ref();
+    const resolvedIndex = ref(0); // records the context when calling doValidate
+
+    async function doValidate() {
+      const index = (resolvedIndex.value += 1);
+      const result = await validateFormData(validatorRef.value, props.value, props.schema, props.customValidator);
+
+      // This is because the returned result of the current validation is not the
+      // most up to date one, we do not need this anymore
+      // (some other validation processes are triggered during this async procedure)
+      if (index !== resolvedIndex.value) return;
+
+      // use shallowRef to allow the SchemaForm component to get this "result" value
+      errorSchemaRef.value = result.errorSchema;
+
+      resolvedValidationRef.value(result); // call resolve
+      resolvedValidationRef.value = undefined; // clean it up
+    }
+
+    watch(
+      () => props.value, // every time when form values change, redo validation and discard the old validation procedures
+      () => {
+        if (resolvedValidationRef.value) {
+          doValidate();
+        }
+      },
+      { deep: true }
+    );
 
     watch(
       () => props.contextRef,
       () => {
         if (props.contextRef) {
           props.contextRef.value = {
-            async validate() {
-              const result = await validateFormData(
-                validatorRef.value,
-                props.value,
-                props.schema,
-                props.customValidator
-              );
-              // use shallowRef to allow the SchemaForm component to get this "result" value
-              errorSchemaRef.value = result.errorSchema;
-              return result;
+            validate() {
+              return new Promise((resolve) => {
+                resolvedValidationRef.value = resolve;
+                doValidate();
+              });
             },
           };
         }
